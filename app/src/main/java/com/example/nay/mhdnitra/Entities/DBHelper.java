cdc28ff5be55212b9e277a64658b50aea8a4ba65
romanstrazanec <roman.strazanec007@gmail.com>
2018-04-06 12:43:09 +0200

    Create DBHelper

---
+++ app/src/main/java/com/example/nay/mhdnitra/Entities/DBHelper.java
@@ -0,0 +1,24 @@
+package com.example.nay.mhdnitra.Entities;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 4;
+    private static final String DATABASE_NAME = "mhd";
+
+    public DBHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}

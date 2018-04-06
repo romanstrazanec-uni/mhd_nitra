b23a39a85fa7f3e1751f88f107ac2a3befd8700f
romanstrazanec <roman.strazanec007@gmail.com>
2018-04-06 13:22:34 +0200

    Create tables in onCreate()

--- app/src/main/java/com/example/nay/mhdnitra/Entities/DBHelper.java
+++ app/src/main/java/com/example/nay/mhdnitra/Entities/DBHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.example.nay.mhdnitra.MyContract;
+
 public class DBHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 4;
     private static final String DATABASE_NAME = "mhd";
@@ -13,12 +15,38 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        String ct = "CREATE TABLE ({0})";
 
+        String SQL_CREATE_TABLE_LINE = String.format(ct,
+                MyContract.Line.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MyContract.Line.COLUMN_LINE + " TEXT");
+
+        String SQL_CREATE_TABLE_STOP = String.format(ct,
+                MyContract.Stop.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MyContract.Stop.COLUMN_NAME + " TEXT");
+
+        String SQL_CREATE_TABLE_LINESTOP = String.format(ct,
+                MyContract.LineStop.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MyContract.LineStop.COLUMN_ID_LINE + " INTEGER, " +
+                MyContract.LineStop.COLUMN_ID_STOP + " INTEGER, " +
+                MyContract.LineStop.COLUMN_NUMBER + " INTEGER");
+
+        String SQL_CREATE_TABLE_TIME = String.format(ct,
+                MyContract.Time.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MyContract.Time.COLUMN_ID_LINESTOP + " INTEGER, " +
+                MyContract.Time.COLUMN_TIME + " TEXT, " +
+                MyContract.Time.COLUMN_WEEKEND + " INTEGER, " +
+                MyContract.Time.COLUMN_HOLIDAYS + " INTEGER");
+
+        db.execSQL(SQL_CREATE_TABLE_LINE);
+        db.execSQL(SQL_CREATE_TABLE_STOP);
+        db.execSQL(SQL_CREATE_TABLE_LINESTOP);
+        db.execSQL(SQL_CREATE_TABLE_TIME);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
 
     }
 }

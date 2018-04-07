acce66b19cb99b09a0c11b826ac9eb2ed9900cfc
romanstrazanec <roman.strazanec007@gmail.com>
2018-04-07 21:38:37 +0200

    Line list layout

--- app/src/main/java/com/example/nay/mhdnitra/MainActivity.java
+++ app/src/main/java/com/example/nay/mhdnitra/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.nay.mhdnitra;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import com.example.nay.mhdnitra.Entities.Line;
 
 public class MainActivity extends AppCompatActivity {
+    SimpleCursorAdapter sca;
+    DBHelper dbh = new DBHelper(this);
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = findViewById(R.id.LineListView);
+        connectAdapter();
+    }
+
+    private void connectAdapter(){
+        sca = new SimpleCursorAdapter(this, R.layout.line_list_layout, dbh.getCursor(MyContract.Line.TABLE_NAME),
+                new String[]{MyContract.Line.COLUMN_ID, MyContract.Line.COLUMN_LINE},
+                new int[]{R.id.line_id, R.id.line}, 0);
+        lv.setAdapter(sca);
     }
 }

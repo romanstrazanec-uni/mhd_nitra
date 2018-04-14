a07a9508ac5d40590c0d7f33d1e5628d75ceed3c
romanstrazanec <roman.strazanec007@gmail.com>
2018-04-14 15:45:59 +0200

    string-arrays in resources for spinners

--- app/src/main/java/com/example/nay/mhdnitra/TimeAddActivity.java
+++ app/src/main/java/com/example/nay/mhdnitra/TimeAddActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -15,6 +16,8 @@ public class TimeAddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_time_add);
         s1 = findViewById(R.id.spinner1);
         s2 = findViewById(R.id.spinner2);
+        connectSpinnerAdapter(s1, R.array.weekend);
+        connectSpinnerAdapter(s2, R.array.holidays);
     }
 
 
@@ -27,5 +30,11 @@ public class TimeAddActivity extends AppCompatActivity {
         i.putExtra("holidays", s2.getSelectedItemPosition());
         setResult(RESULT_OK, i);
         finish();
+    }
+
+    private void connectSpinnerAdapter(Spinner s, int array) {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
     }
 }

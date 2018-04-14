03172e32c9c1f9b87be80e9acd5fd49bab59a405
romanstrazanec <roman.strazanec007@gmail.com>
2018-04-14 03:59:47 +0200

    update timesactivity

---
+++ app/src/main/java/com/example/nay/mhdnitra/TimeAddActivity.java
@@ -0,0 +1,13 @@
+package com.example.nay.mhdnitra;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TimeAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_add);
+    }
+}
--- app/src/main/java/com/example/nay/mhdnitra/TimesActivity.java
+++ app/src/main/java/com/example/nay/mhdnitra/TimesActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class TimesActivity extends AppCompatActivity {
     SimpleCursorAdapter sca;
@@ -27,8 +28,11 @@ public class TimesActivity extends AppCompatActivity {
         Intent i = getIntent();
         lineStopId = i.getLongExtra("line_stop_id", 0);
 
+        TextView tv = findViewById(R.id.times_text_view);
+        tv.setText("Časy linky " + lineStopId);
+
         lv = findViewById(R.id.times_list_view);
-        connectAdapter();
+        connectAdapter("all");
         addOnItemClickListener();
         addOnItemLongClickListener();
     }
@@ -40,27 +44,55 @@ public class TimesActivity extends AppCompatActivity {
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
+        MenuItem tm;
         switch (item.getItemId()) {
             case R.id.time_menu_add:
-                startActivity(new Intent(this, StopsActivity.class));
+                Intent i = new Intent(this, TimeAddActivity.class);
+                i.putExtra("line_stop_id", lineStopId);
+                startActivity(i);
                 return true;
             case R.id.time_menu_all:
-                startActivity(new Intent(this, FavouriteLinesActivity.class));
+                item.setVisible(false);
+                tm = findViewById(R.id.time_menu_weekend);
+                tm.setVisible(true);
+                tm = findViewById(R.id.time_menu_holidays);
+                tm.setVisible(true);
+                connectAdapter("all");
                 return true;
             case R.id.time_menu_weekend:
-                startActivity(new Intent(this, FavouriteLinesActivity.class));
+                item.setVisible(false);
+                tm = findViewById(R.id.time_menu_all);
+                tm.setVisible(true);
+                tm = findViewById(R.id.time_menu_holidays);
+                tm.setVisible(true);
+                connectAdapter("weekend");
                 return true;
             case R.id.time_menu_holidays:
-                startActivity(new Intent(this, FavouriteLinesActivity.class));
+                item.setVisible(false);
+                tm = findViewById(R.id.time_menu_all);
+                tm.setVisible(true);
+                tm = findViewById(R.id.time_menu_weekend);
+                tm.setVisible(true);
+                connectAdapter("holidays");
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    public void connectAdapter() {
+    public void connectAdapter(String filter) {
+        String where = MyContract.Time.COLUMN_ID_LINESTOP + " = " + lineStopId;
+        switch (filter) {
+            case "weekend":
+                where += " AND NOT " + MyContract.Time.COLUMN_WEEKEND + " = 0";
+                break;
+            case "holidays":
+                where += " AND NOT " + MyContract.Time.COLUMN_HOLIDAYS + " = 0";
+                break;
+            default:
+        }
         sca = new SimpleCursorAdapter(this, R.layout.times_list_layout, dbh.getCursor(null, MyContract.Time.TABLE_NAME,
-                null, null, null, MyContract.Time.COLUMN_ID_LINESTOP + " = " + lineStopId,
+                null, null, null, where,
                 null, MyContract.Time.COLUMN_HOUR + ", " + MyContract.Time.COLUMN_MINUTE),
                 new String[]{MyContract.Time.COLUMN_ID, MyContract.Time.COLUMN_HOUR, MyContract.Time.COLUMN_MINUTE},
                 new int[]{R.id.times_id, R.id.hour, R.id.minutes}, 0);
